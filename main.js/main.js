e6bc071405cccc9a9d31cac95367baf2b18629bf
memmedli elvin <memmedlielvindevop@gmail.com>
2022-12-02 03:57:56 +0400

    first

---
+++ main.js/main.js
@@ -0,0 +1,11 @@
+function openRightNav() {
+  document.querySelector("#nav-right").style.left = "0";
+}
+
+function closeRightNav() {
+  document.querySelector("#nav-right").style.left = "-300px";
+}
+
+function navLink() {
+  document.querySelector("#nav-right").style.left = "-300px";
+}
