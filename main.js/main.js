136ed41bcca07ec3325e213bea3f1c6852839445
memmedli elvin <memmedlielvindevop@gmail.com>
2022-12-02 17:49:58 +0400

    finish project

--- main.js/main.js
+++ main.js/main.js
@@ -1,9 +1,11 @@
 function openRightNav() {
   document.querySelector("#nav-right").style.left = "0";
+  document.querySelector("#nav-right").style.boxShadow = "5px 0 10px black";
 }
 
 function closeRightNav() {
   document.querySelector("#nav-right").style.left = "-300px";
+  document.querySelector("#nav-right").style.boxShadow = "none";
 }
 
 function navLink() {
